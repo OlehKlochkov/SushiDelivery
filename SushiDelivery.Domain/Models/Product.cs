49cb918aa75a229aab159c4bb6b05a5d97ca484f
Oleh Klochkov <klochkov.oleh@gmail.com>
2023-03-10 11:35:15 +0200

    Deleted attr

--- SushiDelivery.Domain/Models/Product.cs
+++ SushiDelivery.Domain/Models/Product.cs
@@ -12,7 +12,6 @@ namespace SushiDelivery.Domain.Models
 
         public string Name { get; set; }
 
-        [Column(TypeName = "decimal(5, 2)")]
         public decimal Price { get; set; }
 
         public bool IsAvailable { get; set; }
