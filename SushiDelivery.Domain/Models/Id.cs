579fb6e4da3ae4dbc8057affb7d7099abce47d10
Yaroslav Fursov <yaroslav.fursov@globallogic.com>
2023-07-31 11:00:56 +0300

    Add domain class diagramm

--- SushiDelivery.Domain/Models/Id.cs
+++ SushiDelivery.Domain/Models/Id.cs
@@ -4,44 +4,23 @@
     /// Structure for strongly typing an identifier value
     /// </summary>
     /// <typeparam name="T">Type of Model</typeparam>
-    public struct Id<T> 
+    public readonly struct Id<T> 
     {
         private readonly Guid _value;
 
-        public Id(Guid value)
-        {
-            _value = value;
-        }
+        public Id(Guid value) => _value = value;
 
-        public static explicit operator Guid(Id<T> id)
-        {
-            return id._value;
-        }
+        public static explicit operator Guid(Id<T> id) => id._value;
 
-        public static explicit operator Id<T>(Guid value)
-        {
-            return new Id<T>(value);
-        }
+        public static explicit operator Id<T>(Guid value) => new Id<T>(value);
 
-        public override bool Equals(object? obj)
-        {
-            return obj is Id<T> item && _value.Equals(item._value);
-        }
+        public override bool Equals(object? obj) => obj is Id<T> item && _value.Equals(item._value);
 
-        public override int GetHashCode()
-        {
-            return _value.GetHashCode();
-        }
+        public override int GetHashCode() => _value.GetHashCode();
 
-        public static bool operator ==(Id<T> x, Id<T> y)
-        {
-            return x._value == y._value;
-        }
+        public static bool operator ==(Id<T> x, Id<T> y) => x._value == y._value;
 
-        public static bool operator !=(Id<T> x, Id<T> y)
-        {
-            return !(x == y);
-        }
+        public static bool operator !=(Id<T> x, Id<T> y) => !(x == y);
 
     }
 }
